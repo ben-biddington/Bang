2b9b970dd5f92ce1e94e294f3248b19c04a44f04
Ben Biddington <ben.biddington@gmail.com>
2010-05-22 02:21:49 -0700

    Filter abstraction todo

--- src/Bang/IO/Pipes/FilteredPipe.cs
+++ src/Bang/IO/Pipes/FilteredPipe.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 
 namespace Bang.IO.Pipes {
+        // TODO: refactor filter abstraction. Filter should have a return value -- Some[T] or None[T] perhaps.
 	public class FilteredPipe : SimplePipe {
 		private readonly Func<String, Boolean> _filter;
 
